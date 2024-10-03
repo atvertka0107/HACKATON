53cf6588835cba31a6849495bcf9a17522e457d7
Egor <egor-syrov@bk.ru>
2024-10-03 13:00:43 +0300

    Egor add footer and main

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,18 @@
+import './MainPage.module.css'
+
+export default function MainPage(){
+    return (
+        <div class="container">
+        <div class="text_container">
+            <h1>Проверка гипотез</h1>
+            <p>{}</p>
+            <p>{}</p>
+            <p>{}</p>
+            <p>{}</p>
+        </div>
+        <div class="image_container">
+            <img src="./images/white-offroader-jeep-parking.png" alt="car_logo" class="image" />
+        </div>
+    </div>
+    );
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.module.css'
+
+export default function Footer(){
+    return (
+        <footer>
+        <div>
+            <p>FNInTeam 2024</p>
+            <img src="../refs/Vector.svg" alt="FN_Logo" />
+        </div>
+        </footer>
+    );
+}
